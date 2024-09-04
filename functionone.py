061c832de76e1f321fbc4b869ff6035acfe2b2fb
AsimMairaj <am9162@g.rit.edu>
2024-09-04 15:16:59 +0400

    Add files via upload

---
+++ functionone.py
@@ -0,0 +1,11 @@
+def bday_message():  
+    print ('Greetings. Please answer the following:')
+    name1 = input("What is your name?")
+    Birth_Month1 = input("What is your birth day?")
+    Birth_Day1 = input("What is your birth month?")
+    Birth_Year1 = input("What is your birth year?")
+    print ("Greetings," , name1, "!", "Your birthday is:" , Birth_Day1 , Birth_Month1 , Birth_Year1)
+
+bday_message()
+bday_message()
+bday_message()
